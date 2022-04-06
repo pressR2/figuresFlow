d0c018b5eb10fa04dfc544d695f199d682aa518f
pressR2 <36776929+pressR2@users.noreply.github.com>
2022-04-07 00:32:36 +0200

    [RM-20] - new react project from scripts [RM-22, RM-23] - styled login and sign up components from mockup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn></SignIn>
+      {/* <SignUp></SignUp> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SignUp.tsx
@@ -0,0 +1,34 @@
+import React, { FunctionComponent } from 'react';
+import "./SignIn_SignUp.css";
+
+type SignUpProperties = {
+    // firstName: string;
+    // lastName: string;
+    // email: string;
+    // password: string | number;
+    // retypePassword: string | number;  
+}
+
+const SignUp: FunctionComponent<SignUpProperties> = () => {
+    return(
+        <div className='container'>
+            <div className='form-container'>
+                <form>
+                    <h2>Enter Your Details</h2>
+                    <input placeholder='First Name *'></input>
+                    <input placeholder='Last Name *'></input>
+                    <input placeholder='Email *'></input>
+                    <input placeholder='Password *'></input>
+                    <input placeholder='Retype Password *'></input>
+                    <button className='sign-up-bt' type='submit'>Sign Up</button>
+                    <div className='inner-label'>
+                        <label>Already Have An Account? Then</label>
+                        <a href='#'>Sign In</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default SignUp;
