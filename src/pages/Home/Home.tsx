e8d1f4607f93e1187402e813e7987741aab5c1ca
pressR2 <36776929+pressR2@users.noreply.github.com>
2022-04-09 17:50:10 +0200

    [RM-22] - add new component Home with it's css file

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import homePageImage from '../../images/home-page.jpg';
+import './homePage.css';
 
 function Home() {
   return (
     <div className="Home">
-      Home
+      <div className='home-page-container'>
+        <div className='title'>
+          <h1>Craft Hobby Journal</h1>
+        </div>
+        <img className ='home-page-image' src = {homePageImage} alt ='River Troll'></img>
+        <div className='buttons-container'>
+          <button className='home-page_sign-in'>Sign In</button>
+          <button className='home-page_sign-up'>Sign Up</button>
+          <button className='home-page_demo'>Demo</button>
+        </div>
+      </div>
     </div>
   );
 }
