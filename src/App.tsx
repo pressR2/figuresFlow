e20d95895803368ede84524b6ab155c212a7cbcf
pressR2 <36776929+pressR2@users.noreply.github.com>
2022-04-24 00:58:47 +0200

    [RM-25] - add rudimentary user register and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,46 @@
 import SignIn from "./components/forms/auth/SignIn";
 import SignUp from "./components/forms/auth/SignUp";
+import ForgotPassword from "./components/forms/auth/ForgotPassword";
 import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthProvider from "./services/auth/AuthContext";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route
-                    path="/signIn"
-                    element={<SignIn email="" password="" rememberMe />}
-                />
-                <Route
-                    path="/signUp"
-                    element={
-                        <SignUp
-                            firstName=""
-                            lastName=""
-                            email=""
-                            password=""
-                            retypePassword=""
-                        />
-                    }
-                />
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider
+            email=""
+            password=""
+        >
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route
+                        path="/signIn"
+                        element={<SignIn email="" password="" rememberMe />}
+                    />
+                    <Route
+                        path="/signUp"
+                        element={
+                            <SignUp
+                               firstName=""
+                                lastName=""
+                                email=""
+                                password=""
+                                retypePassword=""
+                            />
+                        }
+                    />
+                    <Route
+                        path="/forgotPassword"
+                        element={
+                            <ForgotPassword
+                                email=""
+                            />
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     );
 }
 
--- src/components/forms/auth/SignIn.tsx
+++ src/components/forms/auth/SignIn.tsx
@@ -1,38 +1,61 @@
-import { FunctionComponent, useState} from "react";
-import { Link } from "react-router-dom";
+import React, { FunctionComponent, useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import "./SignForms.css";
-import { SignInProperties } from "../../../models";
+import { SignInProps } from "../../../models";
+import { useAuth } from "../../../services/auth/AuthContext";
 
-const SignIn: FunctionComponent<SignInProperties> = () => {
-    const { register, handleSubmit } = useForm<SignInProperties>();
-    const [data, setData] = useState("");
+const SignIn: FunctionComponent<SignInProps> = () => {
+    const { register } = useForm<SignInProps>();
+    const authContext  = useAuth();
+    const emailRef = React.useRef<HTMLInputElement>(null);
+    const passwordRef = React.useRef<HTMLInputElement>(null);
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    async function handleSubmit(e: any) {
+        e.preventDefault();
+
+        try {
+            setError("");
+            setLoading(true);
+            await authContext?.signIn(emailRef.current!.value, passwordRef.current!.value);
+            navigate("/");
+        } catch (e: unknown){
+            console.log(e);
+            setError("Faild to log in");
+        }
+        setLoading(false);
+    }
  
     return (
         <div className="container">
             <div className="form-container">
-                <form onSubmit={handleSubmit((values) => setData(JSON.stringify(values)))}>
+                <form onSubmit={handleSubmit}>
                     <h2>User Sign In</h2>
-                    <input {...register("email")} type="email" placeholder="Email *"></input>
-                    <input {...register("password")} type="password" placeholder="Password *"></input>
+                    {console.log(authContext?.currentUser?.email)}
+                    {error && alert(`${error}`)}
+                    <input {...register("email")} type="email" ref={emailRef} placeholder=" Email *"></input>
+                    <input {...register("password")} type="password" ref={passwordRef} placeholder=" Password *"></input>
                     <div className="inner-input">
                         <input {...register("rememberMe")} type="checkbox" id="rememberMe"></input>
                         <label htmlFor="rememberMe">Remember Me</label>
-                        <a href="/#" className="forgot-password_link">
+                        <Link to="/forgotPassword" className="sign_link">
                             Forgot Password?
-                        </a>
+                        </Link>
                     </div>
                     <div className="buttons-container">
-                        <button className="sign-in_bt" type="submit">
+                        <button className="sign-in_bt" type="submit" disabled={loading}>
                             Sign In
                         </button>
-                        <Link to="/#" className="back-to-home">
+                        <Link to="/#" className="back_bt">
                             Back
                         </Link>
                     </div>
                     <div className="inner-label">
                         <p>Don't have an account?</p>
-                        <Link to="/signUp" className="sign-in_link">
+                        <Link to="/signUp" className="sign_link">
                             Click here to create one
                         </Link>
                     </div>
