ac8e419e0797b955ebf768b131828ea45bd0fbf7
pressR2 <36776929+pressR2@users.noreply.github.com>
2022-05-25 02:46:05 +0200

    [RM-27] - add responsive home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,33 +1,29 @@
 import { Link } from "react-router-dom";
-import homePageImage from "../../images/home-page.jpg";
 import "./Home.css";
 
 function Home() {
     return (
-        <div className="home-page-container">
-            <div className="title-and-links">
-                <div className="title">
-                    <h1>FiguresFlow</h1>
-                </div>
-                <div className="buttons-container_home">
-                    <ul className="link-list">
-                        <Link to="/signIn" className="home-page_sign-in">
-                            Sign In
-                        </Link>
-                        <Link to="/signUp" className="home-page_sign-up">
-                            Sign Up
-                        </Link>
-                        <Link to="/demo" className="home-page_demo">
-                            Demo
-                        </Link>
-                    </ul>
+        <div className="home-container">
+            <div className="home-image" role="image">
+                <div className="title-and-btn">
+                    <div className="title">
+                        <h1>FiguresFlow</h1>
+                    </div>
+                    <div className="home-btn-container">
+                        <ul className="btn-list">
+                            <Link to="/signIn" className="home-signin-btn">
+                                Sign In
+                            </Link>
+                            <Link to="/signUp" className="home-signup-btn">
+                                Sign Up
+                            </Link>
+                            <Link to="/demo" className="demo-btn">
+                                Demo
+                            </Link>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <img
-                className="home-page-image"
-                src={homePageImage}
-                alt="River Troll"
-            />
         </div>
     );
 }
