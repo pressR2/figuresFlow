e3e4ad602d05ea5755ca6b362c8bf63d3297fadf
pressR2 <36776929+pressR2@users.noreply.github.com>
2022-05-31 19:34:53 +0200

    [RM-27] - add links and style them based on a specific route

--- src/pages/Dashboard/Sidenav.tsx
+++ src/pages/Dashboard/Sidenav.tsx
@@ -1,17 +1,57 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { SidenavProps } from "../../models";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Sidenav: FunctionComponent<SidenavProps> = (props) => {
+    const { pathname } = useLocation();
+
     return (
         <div className={props.open ? "menu" : "menu hide"}>
             <div className="menu-header">
                 <h2>figuresFlow</h2>
+                <div
+                    className="close-btn"
+                    onClick={props.handleMenu}
+                    role="button"
+                >
+                    <img className="close-icon" />
+                </div>
             </div>
             <hr></hr>
             <ul className="menu-items">
-                <li>Figures</li>
-                <li>Figures 2</li>
-                <li>Figures 3</li>
+                <Link
+                    to="/dashboard"
+                    className={`figures-link ${
+                        pathname === "/dashboard" ? "link" : ""
+                    }`}
+                >
+                    Dashboard
+                </Link>
+                <Link
+                    to="/figures"
+                    className={`figures-link ${
+                        pathname === "/figures" ? "link" : ""
+                    }`}
+                >
+                    Figures
+                </Link>
+                <Link
+                    to="/figures2"
+                    className={`figures-link ${
+                        pathname === "/figures2" ? "link" : ""
+                    }`}
+                >
+                    Figures 2
+                </Link>
+                <Link
+                    to="/figures3"
+                    className={`figures-link ${
+                        pathname === "/figures3" ? "link" : ""
+                    }`}
+                >
+                    Figures 3
+                </Link>
             </ul>
         </div>
     );
