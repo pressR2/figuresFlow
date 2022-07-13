2b6d59be065dd4ee8c244f6889d90b23b4e07bba
pressR2 <36776929+pressR2@users.noreply.github.com>
2022-07-13 18:01:56 +0200

    [RM-29] - add figures filter query logic, grid pagination. Add delete functionality to actions button group, enable/disable button group logic and add styles to toolbar

--- src/pages/FiguresList/DataTable.tsx
+++ src/pages/FiguresList/DataTable.tsx
@@ -1,16 +1,24 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
-import {
+import { 
     DataGrid,
     GridColDef,
     GridRenderCellParams,
-    GridToolbar,
 } from "@mui/x-data-grid";
 import Button from "@mui/material/Button";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/DeleteOutlined";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import { IconButton } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuList from "@mui/material/MenuList";
+import { Link } from "react-router-dom";
 
 interface RowFigureProps {
     id: number;
@@ -20,36 +28,92 @@ interface RowFigureProps {
 
 const DataTable = () => {
     const id = Math.floor(Math.random() * 100);
-    const [tableData, setTableData] = useState<RowFigureProps[]>([]);
+    const options = ["Actions", "Delete"];
 
-    const handleClick = () => {
+    const [tableData, setTableData] = useState<RowFigureProps[]>([]);
+    const [searchQuery, setSearchQuery] = useState<string>("");
+    const [pageSize, setPageSize] = React.useState<number>(2);
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef<HTMLDivElement>(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const [arrIds, setArrIds] = useState<number[]>([]);
+
+    //Handle Add Figure
+    const handleAddFigureClick = () => {
         setTableData([...tableData, { id: id, title: "", date: "" }]);
     };
 
+    //Handle Delete
     const handleDeleteClick = (id: any) => {
-        console.log("d");
         setTableData(tableData.filter((row) => row.id !== id));
     };
 
+    //Handle Filter
+    const handleQueryChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ): void => {
+        setSearchQuery(e.target.value);
+    };
+
+    const filterData = (data: RowFigureProps[]) => {
+        return data.filter(
+            (item) =>
+                item.date.includes(searchQuery) ||
+                item.title.includes(searchQuery)
+        );
+    };
+
+    //Handle Select
+    const handleClick = (data: RowFigureProps[]) => {
+        if (selectedIndex === 1) {
+            setTableData(data.filter((item) => !arrIds.includes(item.id)));
+        }
+    };
+
+    const handleMenuItemClick = (
+        event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+        index: number
+    ) => {
+        setSelectedIndex(index);
+        setOpen(false);
+    };
+
+    const handleToggle = () => {
+        if (arrIds.length === 0) {
+            setOpen(true);
+        }
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleClose = (event: Event) => {
+        if (
+            anchorRef.current &&
+            anchorRef.current.contains(event.target as HTMLElement)
+        ) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
     const columns: GridColDef[] = [
-        {   
+        {
             field: "title",
             headerName: "Title",
             editable: true,
-            width: 130
+            width: 130,
         },
         {
             field: "date",
             headerName: "Date",
             type: "date",
             editable: true,
-            width: 180
+            width: 180,
         },
         {
             field: "actions",
             headerName: "Actions",
             type: "actions",
-            editable: true,
             width: 130,
             renderCell: (params: GridRenderCellParams) => (
                 <div>
@@ -59,11 +123,19 @@ const DataTable = () => {
                     >
                         <DeleteIcon />
                     </IconButton>
-                    <IconButton aria-label="edit">
-                        <EditIcon />
-                    </IconButton>
-                    <IconButton aria-label="view">
+                    <IconButton
+                        component={Link}
+                        to={params.row.id}
+                        aria-label="view"
+                    >
                         <VisibilityIcon />
+                    </IconButton>
+                    <IconButton
+                        component={Link}
+                        to={params.row.id}
+                        aria-label="edit"
+                    >
+                        <EditIcon />
                     </IconButton>
                 </div>
             ),
@@ -78,29 +150,166 @@ const DataTable = () => {
     }, []);
 
     return (
-        <Box sx={{ height: 400, width: 1 }}>
+        <Box
+            sx={{
+                height: "calc(100% -60px)",
+                marginTop: "60px",
+                flex: "1 1 auto",
+                display: "flex",
+            }}
+        >
             <div style={{ height: 400, width: "100%" }}>
-                <div>
-                    <Button onClick={handleClick}>Add Figure</Button>
+                <div
+                    style={{
+                        marginBottom: "17px",
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
+                >
+                    <div
+                        style={{
+                            width: "50%",
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <ButtonGroup
+                            disableElevation
+                            variant="contained"
+                            ref={anchorRef}
+                            aria-label="split button"
+                            style={{
+                                width: 150,
+                                height: "41px",
+                                marginLeft: "17px",
+                                marginRight: "80px",
+                                borderRadius: "4px",
+                            }}
+                        >
+                            <Button
+                                style={{
+                                    backgroundColor: "#1171b8",
+                                }}
+                                onClick={() => handleClick(tableData)}
+                            >
+                                {options[selectedIndex]}
+                            </Button>
+                            <Button
+                                size="small"
+                                style={{
+                                    backgroundColor: "#1171b8",
+                                }}
+                                aria-controls={
+                                    open ? "split-button-menu" : undefined
+                                }
+                                aria-expanded={open ? "true" : undefined}
+                                aria-label="select merge strategy"
+                                aria-haspopup="menu"
+                                onClick={handleToggle}
+                            >
+                                <ArrowDropDownIcon />
+                            </Button>
+                        </ButtonGroup>
+                        <Popper
+                            style={{
+                                zIndex: "1",
+                            }}
+                            open={open}
+                            anchorEl={anchorRef.current}
+                            role={undefined}
+                            transition
+                            disablePortal
+                        >
+                            {({ TransitionProps, placement }) => (
+                                <Grow
+                                    {...TransitionProps}
+                                    style={{
+                                        transformOrigin:
+                                            placement === "bottom"
+                                                ? "center top"
+                                                : "center bottom",
+                                    }}
+                                >
+                                    <Paper>
+                                        <ClickAwayListener
+                                            onClickAway={handleClose}
+                                        >
+                                            <MenuList
+                                                id="split-button-menu"
+                                                autoFocusItem
+                                            >
+                                                {options.map(
+                                                    (option, index) => (
+                                                        <MenuItem
+                                                            key={option}
+                                                            disabled={
+                                                                index === 2
+                                                            }
+                                                            selected={
+                                                                index ===
+                                                                selectedIndex
+                                                            }
+                                                            onClick={(event) =>
+                                                                handleMenuItemClick(
+                                                                    event,
+                                                                    index
+                                                                )
+                                                            }
+                                                        >
+                                                            {option}
+                                                        </MenuItem>
+                                                    )
+                                                )}
+                                            </MenuList>
+                                        </ClickAwayListener>
+                                    </Paper>
+                                </Grow>
+                            )}
+                        </Popper>
+                        <input
+                            style={{
+                                height: 38,
+                                borderRadius: 4,
+                                border: "1px solid #c4c4c4",
+                                paddingLeft: "7px",
+                                fontSize: "1rem",
+                            }}
+                            placeholder="Search"
+                            value={searchQuery}
+                            onChange={handleQueryChange}
+                        />
+                    </div>
+                    <Button
+                        style={{
+                            border: "1px solid",
+                            borderRadius: 4,
+                            width: 150,
+                            height: "44px",
+                            backgroundColor: "#1171b8",
+                            color: "#fff",
+                            marginRight: "17px",
+                        }}
+                        onClick={handleAddFigureClick}
+                    >
+                        Add Figure
+                    </Button>
                 </div>
                 <DataGrid
-                    rows={tableData}
+                    rows={filterData(tableData)}
                     columns={columns}
                     editMode="row"
-                    pageSize={30}
-                    rowsPerPageOptions={[2, 5, 10]}
+                    pageSize={pageSize}
+                    onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                    rowsPerPageOptions={[2, 10, 20]}
+                    pagination
+                    {...tableData}
                     checkboxSelection
-                    experimentalFeatures={{ newEditingApi: true }}
-                    disableColumnFilter
-                    disableColumnSelector
-                    disableDensitySelector
-                    components={{ Toolbar: GridToolbar }}
-                    componentsProps={{
-                        toolbar: {
-                            showQuickFilter: true,
-                            quickFilterProps: { debounceMs: 500 },
-                        },
+                    disableSelectionOnClick
+                    onSelectionModelChange={(id: any) => {
+                        setArrIds(id);
                     }}
+                    experimentalFeatures={{ newEditingApi: true }}
                 />
             </div>
         </Box>
