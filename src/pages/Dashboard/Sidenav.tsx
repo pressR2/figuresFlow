092c68a9953afdae42dcaa17438177ffc92dfdbb
pressR2 <36776929+pressR2@users.noreply.github.com>
2022-10-15 16:38:18 +0200

    [RM-31] feat: sidenav open by default on desktop, close on mobile

--- src/pages/Dashboard/Sidenav.tsx
+++ src/pages/Dashboard/Sidenav.tsx
@@ -1,66 +1,130 @@
 import { Link, useLocation } from "react-router-dom";
 import { SidenavProps } from "../../models";
+import MediaQuery from "react-responsive";
 
 const SideNav = ({ open, handleMenu }: SidenavProps) => {
     const { pathname } = useLocation();
 
     return (
         <aside>
-            <div className={open ? "menu" : "menu hide"}>
-                <header className="menu-header">
-                    <h2>figures<span>Flow</span></h2>
-                    <button
-                        className="close-btn"
-                        onClick={handleMenu}
-                        type="button"
-                    >
-                        <img className="close-icon" alt="" />
-                    </button>
-                </header>
-                <hr></hr>
-                <ul className="menu-items">
-                    <Link
-                        to="/figuresList"
-                        className={`figures-link ${
-                            pathname === "/figuresList" ? "link" : ""
-                        }`}
-                    >
-                        Figures List
-                    </Link>
-                    <Link
-                        to="/dashboard"
-                        className={`figures-link ${
-                            pathname === "/dashboard" ? "link" : ""
-                        }`}
-                    >
-                        Dashboard
-                    </Link>
-                    <Link
-                        to="/figures"
-                        className={`figures-link ${
-                            pathname === "/figures" ? "link" : ""
-                        }`}
-                    >
-                        Figures
-                    </Link>
-                    <Link
-                        to="/figures2"
-                        className={`figures-link ${
-                            pathname === "/figures2" ? "link" : ""
-                        }`}
-                    >
-                        Figures 2
-                    </Link>
-                    <Link
-                        to="/figures3"
-                        className={`figures-link ${
-                            pathname === "/figures3" ? "link" : ""
-                        }`}
-                    >
-                        Figures 3
-                    </Link>
-                </ul>
-            </div>
+            <MediaQuery maxWidth={550}>
+                <div className={open ? "menu hide" : "menu"}>
+                    <header className="menu-header">
+                        <h2>
+                            figures<span>Flow</span>
+                        </h2>
+                        <button
+                            className="close-btn"
+                            onClick={handleMenu}
+                            type="button"
+                        >
+                            <img className="close-icon" alt="" />
+                        </button>
+                    </header>
+                    <hr></hr>
+                    <ul className="menu-items">
+                        <Link
+                            to="/figuresList"
+                            className={`figures-link ${
+                                pathname === "/figuresList" ? "link" : ""
+                            }`}
+                        >
+                            Figures List
+                        </Link>
+                        <Link
+                            to="/dashboard"
+                            className={`figures-link ${
+                                pathname === "/dashboard" ? "link" : ""
+                            }`}
+                        >
+                            Dashboard
+                        </Link>
+                        <Link
+                            to="/figures"
+                            className={`figures-link ${
+                                pathname === "/figures" ? "link" : ""
+                            }`}
+                        >
+                            Figures
+                        </Link>
+                        <Link
+                            to="/figures2"
+                            className={`figures-link ${
+                                pathname === "/figures2" ? "link" : ""
+                            }`}
+                        >
+                            Figures 2
+                        </Link>
+                        <Link
+                            to="/figures3"
+                            className={`figures-link ${
+                                pathname === "/figures3" ? "link" : ""
+                            }`}
+                        >
+                            Figures 3
+                        </Link>
+                    </ul>
+                </div>
+            </MediaQuery>
+            <MediaQuery minWidth={551}>
+                <div className={open ? "menu" : "menu hide"}>
+                    <header className="menu-header">
+                        <h2>
+                            figures<span>Flow</span>
+                        </h2>
+                        <button
+                            className="close-btn"
+                            onClick={handleMenu}
+                            type="button"
+                        >
+                            <img className="close-icon" alt="" />
+                        </button>
+                    </header>
+                    <hr></hr>
+                    <ul className="menu-items">
+                        <Link
+                            to="/figuresList"
+                            className={`figures-link ${
+                                pathname === "/figuresList" ? "link" : ""
+                            }`}
+                        >
+                            Figures List
+                        </Link>
+                        <Link
+                            to="/dashboard"
+                            className={`figures-link ${
+                                pathname === "/dashboard" ? "link" : ""
+                            }`}
+                        >
+                            Dashboard
+                        </Link>
+                        <Link
+                            to="/figures"
+                            className={`figures-link ${
+                                pathname === "/figures" ? "link" : ""
+                            }`}
+                        >
+                            Figures
+                        </Link>
+                        <Link
+                            to="/figures2"
+                            className={`figures-link ${
+                                pathname === "/figures2" ? "link" : ""
+                            }`}
+                        >
+                            Figures 2
+                        </Link>
+                        <Link
+                            to="/figures3"
+                            className={`figures-link ${
+                                pathname === "/figures3" ? "link" : ""
+                            }`}
+                        >
+                            Figures 3
+                        </Link>
+                    </ul>
+                </div>
+            </MediaQuery>
         </aside>
     );
 };
